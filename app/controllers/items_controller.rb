43c00353f8ddb2e8555c38229df5027231bd35f4
upbeatvision <info@upbeatvision.com>
2015-10-26 10:36:36 +0000

    second

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,6 @@ class ItemsController < ApplicationController
   def new
     if params[:q]
       response = Amazon::Ecs.item_search(params[:q] , 
-                                  :search_index => 'All' , 
                                   :response_group => 'Medium' , 
                                   :country => 'jp')
       @amazon_items = response.items
